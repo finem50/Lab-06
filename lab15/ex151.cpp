7673283431651b9dc7463a6603ae667ce539bfa1
Finem50 <Finem50@live.com>
2018-10-06 17:59:00 -0500

    Completing exercise to implement a date struct with a linked list.

--- lab15/ex151.cpp
+++ lab15/ex151.cpp
@@ -6,33 +6,48 @@ using namespace std;
 
 struct Node{
   string item;
-  int count;
+  int n_day, n_month, n_year;
   Node *link;
+};
+
+struct Date{
+  int day;
+  int month;
+  int year;
 };
 
 typedef Node* NodePtr;
 
-void head_insert(NodePtr& head, string an_item, int a_number);
+void head_insert(NodePtr& head, string an_item, int a_day, int a_month,
+    int a_year);
 void show_list(NodePtr& head);
 
 int main(){
+  struct Date Rolls = {8, 10, 2003};
+
+  struct Date Jam = {9, 12, 2003};
+
+  struct Date Tea = {1, 1, 2010};
 
   NodePtr head = NULL;
-  head_insert(head, "Tea", 2);
-  head_insert(head, "Jam", 3);
-  head_insert(head, "Rolls", 10);
+  head_insert(head, "Tea", Tea.day, Tea.month, Tea.year);
+  head_insert(head, "Jam", Jam.day, Jam.month, Jam.year);
+  head_insert(head, "Rolls", Rolls.day, Rolls.month, Rolls.year);
 
   show_list(head);
 
   return 0;
 }
 
-void head_insert(NodePtr& head, string an_item, int a_number){
+void head_insert(NodePtr& head, string an_item, int a_day, int a_month,
+                  int a_year){
   NodePtr temp_ptr;
   temp_ptr = new Node;
 
   temp_ptr -> item = an_item;
-  temp_ptr -> count = a_number;
+  temp_ptr -> n_day = a_day;
+  temp_ptr -> n_month = a_month;
+  temp_ptr -> n_year = a_year;
 
   temp_ptr -> link = head;
   head = temp_ptr;
@@ -43,7 +58,9 @@ void show_list(NodePtr& head){
 
   while(here != NULL){
     cout << here -> item << "\t";
-    cout << here -> count << endl;
+    cout << here -> n_day << "/";
+    cout << here -> n_month << "/";
+    cout << here -> n_year << endl;
 
     here = here -> link;
   }
